b7e88f732d59ed175f9aec35528c3b8b83c3a1fc
Rahim Alwer <rahim_alwer@hotmail.com>
2020-08-29 14:40:05 +1000

    feat(providers/file): watch source elements for changes and call load

--- packages/core/src/components.d.ts
+++ packages/core/src/components.d.ts
@@ -2184,6 +2184,10 @@ declare namespace LocalJSX {
         "muted"?: boolean;
         "onVLoadStart"?: (event: CustomEvent<void>) => void;
         /**
+          * Emitted when the child <source> elements are modified.
+         */
+        "onVSrcSetChange"?: (event: CustomEvent<void>) => void;
+        /**
           * The playback rates that are available for this media.
          */
         "playbackRates"?: number[];
